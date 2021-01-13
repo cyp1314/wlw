6a626764b6392d34d4403eeaf11326a6235bf038
yongpeng.chen <yongpeng.chen@hcr.com.cn>
2021-01-13 10:38:22 +0800

    Initial commit

---
+++ src/test/java/com/chen/wlw/WlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.wlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
